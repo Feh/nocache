75f4ed13d55ae3bf2e87ad0d7c070e971740eb0f
Julius Plenz <julius@plenz.com>
2013-05-10 20:48:30 +0200

    Revert "support for kfreebsd and old FreeBSD systems."
    This reverts commit 0a214bdbb24f18c62f44f97b3ee3cb9bce15daf6.
    
    Apparently, building nocache on kfreebsd still fails, and anyway the linker
    warns that posix_fadvise will always fail. So so wait until kfreebsd is
    up-to-date with the POSIX standard. (See issue #12.)

--- cachedel.c
+++ cachedel.c
@@ -1,14 +1,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#if __POSIX_VISIBLE < 200112
-#define POSIX_FADV_NORMAL       0       /* no special treatment */
-#define POSIX_FADV_RANDOM       1       /* expect random page references */
-#define POSIX_FADV_SEQUENTIAL   2       /* expect sequential page references */
-#define POSIX_FADV_WILLNEED     3       /* will need these pages */
-#define POSIX_FADV_DONTNEED     4       /* dont need these pages */
-#define POSIX_FADV_NOREUSE      5       /* access data only once */
-#endif
 #include <error.h>
 #include <stdio.h>
 #include <stdlib.h>
